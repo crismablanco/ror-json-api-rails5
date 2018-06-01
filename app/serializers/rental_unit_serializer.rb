9d39b10dcab9db4b4b14eff4954b7bf2908c9cd6
crismablanco <crismablanco@gmail.com>
2018-06-01 18:32:51 +0000

    add price and price per room methods on serializer

--- app/serializers/rental_unit_serializer.rb
+++ app/serializers/rental_unit_serializer.rb
@@ -1,4 +1,12 @@
 class RentalUnitSerializer < ActiveModel::Serializer
   attributes :id, :address, :rooms, :bathrooms, :price, :price_per_room
   belongs_to :user
+  
+  def price
+    object.price_cents
+  end
+  
+  def price_per_room
+    object.price_cents
+  end
 end
